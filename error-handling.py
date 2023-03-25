4614261e83942601f884eca78565e154560c409b
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-25 23:18:22 +0800

    fix syntax error

--- error-handling.py
+++ error-handling.py
@@ -2,16 +2,18 @@
 import sys #to import system module
 
 #ZeroDivisionError:
-x = int(input("Enter x: "))
-y = int(input("Enter y: "))
+try:
+    x = int(input("Enter x: "))
+    y = int(input("Enter y: "))
+except ValueError:
+    print("Error: Enter a number only")
+    sys.exit(1)
 
 try:
     result = x/y
 except ZeroDivisionError:
     print("Error: Cannot divide by 0")
     sys.exit(1) #allow the system to exit the program
-except ValueError:
-    print("Error: Enter a number only")
-    sys.exit(1)
+
 
 print(f"{x} / {y} = {result}")
