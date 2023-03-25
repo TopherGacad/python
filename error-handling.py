1d6ead8c762b34f11e44ff3e993ce2548274684f
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-25 23:14:48 +0800

    added except value error

--- error-handling.py
+++ error-handling.py
@@ -1,13 +1,17 @@
 #simple error handling
 import sys #to import system module
 
+#ZeroDivisionError:
 x = int(input("Enter x: "))
 y = int(input("Enter y: "))
 
 try:
     result = x/y
 except ZeroDivisionError:
-    print("Error cannot divide by 0")
+    print("Error: Cannot divide by 0")
     sys.exit(1) #allow the system to exit the program
+except ValueError:
+    print("Error: Enter a number only")
+    sys.exit(1)
 
 print(f"{x} / {y} = {result}")
